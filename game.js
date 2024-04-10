c5fe956dea86e6139e252be8b15a8b6222d35b85
MikeYoulend <mikeyoulend@gmail.com>
2024-04-10 19:52:55 +0200

    Simon Game

---
+++ game.js
@@ -0,0 +1,22 @@
+//alert("Benvenuto in Simon Game")
+
+let gamePattern = []
+let buttonColours = ["red", "blue", "yellow", "green"]
+
+function nextSequence(){
+    let randomNumber = (Math.floor(Math.random() * 4));
+    console.log(randomNumber);
+
+    let randomChosenColour = buttonColours[randomNumber];
+    console.log(randomChosenColour);
+    gamePattern.push(randomChosenColour)
+    console.log(gamePattern);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
+}
+
+nextSequence();
+
